166b09f788452878e30461484d9120fdd6bf7bd0
darkroom2 <rkomorowski97@gmail.com>
2022-10-16 22:06:00 +0200

    prototype state changing

--- src/arkanoidlike.cpp
+++ src/arkanoidlike.cpp
@@ -1,18 +1,37 @@
 #include "../libs/Framework/inc/Framework.h"
+#include "../include/Game.h"
+#include <cxxopts.hpp>
+#include <iostream>
+#include <filesystem>
+
 
 /* Test Framework realization */
-class MyFramework : public Framework {
+class ArkanoidLike : public Framework {
+private:
+    uint16_t window_width;
+    uint16_t window_height;
+    bool window_fullscreen;
 
 public:
+    explicit ArkanoidLike(
+            uint16_t windowWidth = 600,
+            uint16_t windowHeight = 800,
+            bool windowFullscreen = false
+    ) : window_width(windowWidth),
+        window_height(windowHeight),
+        window_fullscreen(windowFullscreen) {
+    }
+
+    std::unique_ptr<Game> game;
 
     virtual void PreInit(int &width, int &height, bool &fullscreen) {
-        width = 320;
-        height = 200;
-        fullscreen = false;
+        width = window_width;
+        height = window_height;
+        fullscreen = window_fullscreen;
     }
 
     virtual bool Init() {
-
+        game = std::make_unique<Game>();
         return true;
     }
 
@@ -21,7 +40,8 @@ public:
     }
 
     virtual bool Tick() {
-        drawTestBackground();
+        auto ticks = getTickCount();
+        game->update(ticks);
         return false;
     }
 
@@ -30,20 +50,66 @@ public:
     }
 
     virtual void onMouseButtonClick(FRMouseButton button, bool isReleased) {
-
+        if (button == FRMouseButton::LEFT && isReleased) {
+            if (game->currentState == game->states.at("StartGameState").get())
+                game->setCurrentState("GameplayGameState");
+        }
     }
 
     virtual void onKeyPressed(FRKey k) {
     }
 
     virtual void onKeyReleased(FRKey k) {
+
     }
 
     virtual const char *GetTitle() override {
-        return "Arcanoid";
+        return "Arcanoid Like";
+    }
+
+    virtual const char *GetDescription() {
+        return "This is a game where you have to overcome the wall using a ball to reach victory!";
+    }
+
+    virtual void SetWindowSettings(int w, int h, bool fullscreen) {
+        window_width = w;
+        window_height = h;
+        window_fullscreen = fullscreen;
     }
 };
 
-int main(int argc, char *argv[]) {
-    return run(new MyFramework);
+
+int main(int argc, const char *argv[]) {
+    auto arkanoidlike = std::make_unique<ArkanoidLike>();
+    try {
+        std::unique_ptr<cxxopts::Options> allocated(new cxxopts::Options(argv[0], arkanoidlike->GetDescription()));
+        auto &options = *allocated;
+
+        options.add_options()
+                ("h,help", "Print help")
+                ("w,window", "Window resolution [W,H] eg. -w 800,600", cxxopts::value<std::vector<int>>());
+
+        auto result = options.parse(argc, argv);
+
+        if (result.count("help")) {
+            std::cout << options.help() << std::endl;
+            return 0;
+        }
+
+        if (result.count("window")) {
+            const auto values = result["window"].as<std::vector<int>>();
+            if (values.size() != 2) {
+                std::cout << "provide two arguments [w,h] eg. 800,600" << std::endl;
+                return -1;
+            }
+            arkanoidlike->SetWindowSettings(values[0], values[1], false);
+        } else {
+            arkanoidlike->SetWindowSettings(0, 0, true);
+        }
+    }
+    catch (const cxxopts::exceptions::exception &e) {
+        std::cout << "error parsing options: " << e.what() << std::endl;
+        return -1;
+    }
+    return run(arkanoidlike.get());
 }
