39bfdaedccecca7ad5f9728deb28c1f727873b2f
darkroom2 <rkomorowski97@gmail.com>
2022-10-17 17:15:32 +0200

    add SpriteLoader

---
+++ include/SpriteLoader.h
@@ -0,0 +1,14 @@
+//
+// Created by darkroom2 on 17/10/22.
+//
+
+#ifndef ARKANOID_LIKE_SPRITELOADER_H
+#define ARKANOID_LIKE_SPRITELOADER_H
+
+
+class SpriteLoader {
+
+};
+
+
+#endif //ARKANOID_LIKE_SPRITELOADER_H
---
+++ src/SpriteLoader.cpp
@@ -0,0 +1,6 @@
+//
+// Created by darkroom2 on 17/10/22.
+//
+
+#include "../include/SpriteLoader.h"
+
