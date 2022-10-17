705387da859d7c4e18b0ada852bdf9c74a33037c
darkroom2 <rkomorowski97@gmail.com>
2022-10-17 12:24:56 +0200

    pass along path to executable

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -2,20 +2,17 @@
 // Created by darkroom2 on 16/10/22.
 //
 
-#include <filesystem>
 #include "../include/GameState.h"
 #include "../include/Game.h"
 
 
-GameState::GameState(Game *game) : game(game) {}
+GameState::GameState(Game *game, std::string &execPath) : game(game), execPath(execPath) {}
 GameState::~GameState() = default;
 
 
-StartGameState::StartGameState(Game *game) : GameState(game) {}
+StartGameState::StartGameState(Game *game, std::string &execPath) : GameState(game, execPath) {}
 
-void StartGameState::update(unsigned int i) {
-
-}
+void StartGameState::update(unsigned int i) {}
 
 void StartGameState::handleMouseMove(int x, int y, int xrel, int yrel) {}
 
@@ -28,20 +25,15 @@ void StartGameState::handleMouseKey(FRMouseButton button, bool released) {
     }
 }
 
-GameplayGameState::GameplayGameState(Game *game) : GameState(game), map(std::make_unique<Map>()) {}
+
+GameplayGameState::GameplayGameState(Game *game, std::string &execPath) : GameState(game, execPath), map(std::make_unique<Map>()) {}
 
 void GameplayGameState::update(unsigned int i) {
     map->update(i);
 }
 
-void GameplayGameState::handleKey(FRKey key, bool pressed) {
+void GameplayGameState::handleKey(FRKey key, bool pressed) {}
 
-}
+void GameplayGameState::handleMouseMove(int x, int y, int xrel, int yrel) {}
 
-void GameplayGameState::handleMouseMove(int x, int y, int xrel, int yrel) {
-
-}
-
-void GameplayGameState::handleMouseKey(FRMouseButton button, bool released) {
-
-}
+void GameplayGameState::handleMouseKey(FRMouseButton button, bool released) {}
