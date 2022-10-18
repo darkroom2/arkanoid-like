ab3efccd3ace03fb58147e37156aa1c6eac4c91a
darkroom2 <rkomorowski97@gmail.com>
2022-10-18 03:17:28 +0200

    add to map

--- src/Map.cpp
+++ src/Map.cpp
@@ -9,25 +9,39 @@
 
 
 Map::Map(int x, int y, int lines, int columns) : Entity(x, y) {
-    auto s = createSprite("brick-green-normal.png");
-    int bw{};
-    int bh{};
-    getSpriteSize(s, bw, bh);
-    double hw_ratio = (double) bh / (double) bw;
     getScreenSize(width, height);
+
     int brick_width = (int) std::round((double) width / (double) columns);
-    int brick_height = (int) std::round(hw_ratio * brick_width);
-    setSpriteSize(s, brick_width, brick_height);
+
+    Brick brick = Brick(0, 0, EntityColor::RED);
+    brick.setDimensions(brick_width, 0);
+    for (int i = 0; i < columns; ++i) {
+        bricks.push_back(brick);
+    }
+
+    brick = Brick(0, 0, EntityColor::GREEN);
+    brick.setDimensions(brick_width, 0);
+    for (int i = 0; i < (lines - 1) * columns; ++i) {
+        bricks.push_back(brick);
+    }
+
     for (int i = 0; i < lines; ++i) {
         for (int j = 0; j < columns; ++j) {
-            auto brick = std::make_unique<Brick>(j * brick_width, i * brick_height);
-            bricks.push_back(std::move(brick));
+            bricks[i * columns + j].setPosition(brick.width * j, brick.height * i);
         }
     }
+
+    int perkWidth = (int) std::round((double) width / (double) columns);
+    Perk *perk = new Perk(22, 22);
+    perk->setDimensions(perkWidth, 0);
+    entities.push_back(perk);
 }
 
 void Map::update(unsigned int i) {
-    for (const auto &b: bricks) {
-        b->update(i);
+    for (auto &b: bricks) {
+        b.update(i);
+    }
+    for (auto &e: entities) {
+        e->update(i);
     }
 }
